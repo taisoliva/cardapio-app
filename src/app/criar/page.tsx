35975ea1e3b37b75a725cf754163f9af789b617c
tais <tais15oliva@gmail.com>
2023-10-25 17:53:23 -0300

    fix: fixing bug when sending a menu on the form

---
+++ src/app/criar/page.tsx
@@ -0,0 +1,8 @@
+import { SettingTabs } from "@/components/SettingTabs";
+
+export default async function Forms() {
+
+    return (
+        <SettingTabs />
+    )
+}
--- src/components/Forms/Menu/index.tsx
+++ src/components/Forms/Menu/index.tsx
@@ -2,7 +2,9 @@
 
 import { postMenu } from "@/services/menuApi";
 import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
-import { useState } from "react";
+import axios from "axios";
+import { useState, FormEvent } from "react";
+
 
 interface Options {
     diurno: boolean;
@@ -12,9 +14,9 @@ interface Options {
 
 export default function FormMenu() {
 
-    const [name, setName] = useState<string> ('')
+    const [name, setName] = useState<string>('')
     const [selectedOption, setSelectedOption] = useState<string>('diurno')
-    const [isError, setError] = useState<boolean> (false)
+    const [isError, setError] = useState<boolean>(false)
 
     const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
@@ -24,33 +26,41 @@ export default function FormMenu() {
         setSelectedOption(event.target.value);
     };
 
-     const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault(); 
-        if(name === ''){
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (name === '') {
             setError(true)
         } else {
+
             setError(false)
         }
-        
+
         const body = {
             name,
             type: selectedOption
         }
 
-        console.log(body)
+        try {
+            const promise = await postMenu(body)
+           console.log(promise)
+        } catch (error) {
+
+            console.log("deu ruim", error)
+        }
     };
 
     return (
         <form className="m-5" onSubmit={handleSubmit}>
-            <TextField style={{ width: '100%' }} label="Nome do Cardápio" type="text" variant="outlined" value={name} onChange={handleNameChange}
+            <TextField style={{ width: '100%' }} name='title' label="Nome do Cardápio" type="text" variant="outlined" value={name} onChange={handleNameChange}
                 error={isError}
                 helperText={isError && "Preencha com o nome"} />
             <div className="mt-2 ">
                 <FormControl>
                     <FormLabel> Escolha a opção de horário </FormLabel>
                     <div className="flex">
-                        <RadioGroup row aria-labelledby="demo-radio-buttons-group-label" defaultValue="diurno" name="radio-buttons-group" 
-                        value={selectedOption} onChange={handleOptionChange}>
+                        <RadioGroup row aria-labelledby="demo-radio-buttons-group-label" defaultValue="diurno" name="radio-buttons-group"
+                            value={selectedOption} onChange={handleOptionChange}>
                             <FormControlLabel value="diurno" control={<Radio />} label="Diurno" />
                             <FormControlLabel value="noturno" control={<Radio />} label="Noturno" />
                         </RadioGroup>
