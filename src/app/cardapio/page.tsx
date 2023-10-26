4b525ce10b59cfc9d920a9f55d1e0f43d4cfa222
tais <tais15oliva@gmail.com>
2023-10-26 16:06:26 -0300

    deleting unused folders

--- src/app/cardapio/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Cardapio() {
-    return (
-     <div className="">
-        <h1> ola </h1>
-     </div>
-    )
-  }
--- src/app/produtos/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Cardapio() {
-    return (
-     <div className="">
-        <h1> ola id</h1>
-     </div>
-    )
-  }
